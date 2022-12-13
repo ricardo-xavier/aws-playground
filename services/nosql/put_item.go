b5a096afe3f6eef8c670cac4f3c4deee97fa167a
ricardo-xavier <ricardo.costa.xavier@gmail.com>
2022-12-12 22:14:20 -0300

    wip: demo load

--- services/nosql/put_item.go
+++ services/nosql/put_item.go
@@ -1,12 +1,47 @@
 package main
 
 import (
-"fmt"
+    "os"
     "nosql/model"
 )
 
 func PutItem(request model.PutItemRequest) {
-    for k, v := range request.Items {
-        fmt.Printf("%v = [%v]\n", k, v)
+    schema := model.ReadSchema(request.Table + ".sch")
+    dat, err := os.OpenFile(request.Table + ".dat", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        panic(err)
     }
+
+    var values [256]string
+    save := false
+    for name, attr := range request.Items {
+        idx := -1
+        for i, field := range schema.Attributes {
+            if field.Name == name {
+                idx = i
+                break
+            }
+        }
+        if idx == -1 {
+            newAttr := model.Attribute {
+                Name: name,
+                Type: attr.AttributeType,
+            }
+            idx = len(schema.Attributes)
+            schema.Attributes = append(schema.Attributes, newAttr)
+            save = true
+        }
+        values[idx] = attr.Value
+    }
+    if save {
+        schema.Write(request.Table + ".sch")
+    }
+    for i := 0; i < len(schema.Attributes); i++ {
+        if i > 0 {
+            dat.WriteString("|")
+        }
+        dat.WriteString(values[i])
+    }
+    dat.WriteString("\n")
+    dat.Close()
 }
